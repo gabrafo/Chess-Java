9cfa7fadd09d0aaf6cff103988b9033d92ba4ac6
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2024-04-07 17:23:12 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -35,9 +34,14 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        // Colocar peças informando coordenadas no sistema do xadrez e não a posição da matriz
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup(){ // Método responsável por iniciar a partida de xadrez, colocando as peças
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,47 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+        if(column<'a' || column > 'h' || row<1 || row > 8){
+            throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    protected Position toPosition(){
+        // Método para transformar de posição na matriz p/ posição no tabuleiro
+
+        // Primeira linha da matriz (linha 8 do tabuleiro) é 0,0
+        // Coluna a = 0, então 'a' - 'a' = 0, 'b' - 'a' = 1, 'c' - 'a' = 2, etc
+
+        // matrix_row = 8 - chess_row
+        // matrix_column = chess_column - 'a'
+
+        return new Position(8-row, column-'a');
+    }
+
+    protected static ChessPosition frompPosition(Position position){
+        // Método para transformar de posição no tabuleiro p/ posição na matriz
+
+        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString(){
+        return "" + column + row; // String vazio força o interpretador a concatenar os dois valores e não somá-los
+    }
+}
